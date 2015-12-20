27101a3a5318254cc79fe0a5e4168848edd04f5b
Eric Metcalf <ehm216@lehigh.edu>
2015-12-20 16:29:45 -0500

    added features

--- app/src/main/java/me/ericmetcalf/messanger/RecieveMessageActivity.java
+++ app/src/main/java/me/ericmetcalf/messanger/RecieveMessageActivity.java
@@ -1,30 +1,28 @@
 
 package me.ericmetcalf.messanger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class RecieveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "messageValue";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recieve_message);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
--- app/src/main/java/me/ericmetcalf/messanger/SendMessage.java
+++ app/src/main/java/me/ericmetcalf/messanger/SendMessage.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
-
+import android.widget.EditText;
 
 public class SendMessage extends AppCompatActivity {
 
@@ -19,10 +19,16 @@ public class SendMessage extends AppCompatActivity {
 
     //claiming the method when the button is pressed
     public void onSendMessage(View view){
-        Intent intent = new Intent(this, RecieveMessageActivity.class);
-        startActivity(intent);
+        EditText et = (EditText) findViewById(R.id.message);
+        String value = et.getText().toString();
+       // Intent intent = new Intent(this, RecieveMessageActivity.class);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, value);
+        String chooserTitle = getString(R.string.chooser);
+        Intent choosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(choosenIntent);
 
     }
 
 }
-
